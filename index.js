40acf42a94c84fcfc479898fe6a58f21f6024414
otto18 <otto@ceramic.global>
2024-02-05 17:26:31 -0500

    everything before adding events

---
+++ index.js
@@ -0,0 +1,91 @@
+// References
+const COHORT = "2401-FSA-ET-WEB-FT-SF";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+
+const eventList = document.querySelector("#events");
+const addEventForm = document.querySelector("#addEvent");
+
+//State
+const state = {
+  events: [],
+};
+
+/**
+ * Update state with events from API
+ */
+async function getEvents() {
+  try {
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    state.events = json.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * Sync state with the API and rerender
+ */
+async function render() {
+  await getEvents();
+  renderEvents();
+}
+
+//Render
+/**
+ * Render events from state
+ */
+function renderEvents() {
+  if (!state.events.length) {
+    eventList.innerHTML = "<li>No events.</li>";
+    return;
+  }
+
+  const eventCards = state.events.map((event) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <h2>${event.name}</h2>
+        <p2>${event.description}</p2>
+        <p2>${event.date}</p2>
+        <p>${event.location}</p>
+        <button>Delete</button>
+      `;
+    return li;
+  });
+
+  eventList.replaceChildren(...eventCards);
+}
+
+/**
+ * Ask the API to create a new event based on form data
+ * @param {Event} event
+ */
+async function addEvent(event) {
+  event.preventDefault();
+
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: addEventForm.name.value,
+        description: addEventForm.description.value,
+        date: addEventForm.date.value,
+        location: addEventForm.location.value,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to create event");
+    }
+
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//Script
+
+render();
+addEventForm.addEventListener("submit", addEvent);
